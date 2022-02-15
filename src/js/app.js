aea73b248f2cdba29fd4c4d493eb62149f50b80d
kupermanak <kupermanak@gmail.com>
2022-02-15 00:35:57 +0000

    1st udate

--- src/js/app.js
+++ src/js/app.js
@@ -33,10 +33,12 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name == null ? "Maria" : variables.name} ${
+    variables.lastname == null ? "Becerra" : variables.lastname
+  }</h1>
+          <h2>${variables.role == null ? "Game player" : variables.role}</h2>
+          <h3>${variables.city == null ? "Buenos Aires" : variables.city}</h3>
+          <ul class=${variables.socialMediaPosition}>
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
